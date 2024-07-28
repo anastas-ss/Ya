a655c058f9878d61b9cff16e588466195a5bb3b8
anastas-ss <s02210497@gse.cs.msu.ru>
2024-07-28 22:20:56 +0300

    DZ1_01

---
+++ DZ_1/ex1.py
@@ -0,0 +1,18 @@
+#A - кондиционер
+type_work = ["freeze", "heat", "auto", "fun"]
+t_room, t_cond = map(int, input().split())
+ttype = input()
+if ttype == type_work[3]:
+    print(t_room)
+elif ttype == type_work[2]:
+    print(t_cond)
+elif ttype == type_work[1]:
+    if t_room < t_cond:
+        print(t_cond)
+    else:
+        print(t_room)
+elif ttype == type_work[0]:
+    if t_room > t_cond:
+        print(t_cond)
+    else:
+        print(t_room)
